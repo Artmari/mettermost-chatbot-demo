ea8051ad9f0e01cd51857c33fee474161c450a9a
Maria Artymovych <artmari@Marias-Air.home>
2024-07-05 01:00:18 +0200

    removed unused code

--- src/logging.ts
+++ src/logging.ts
@@ -3,5 +3,4 @@ import {Log} from "debug-level";
 Log.options({json: true, colors: true})
 Log.wrapConsole('bot-ws', {level4log: 'INFO'})
 export const  botLog = new Log('bot')
-export const openAILog = new Log('open-ai')
 export const matterMostLog = new Log('mattermost')
